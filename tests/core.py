55ffff6e1e6f73dc374dcc2289d564bc7cf719a3
Vitor Macêdo <vitor.costamacedo1@gmail.com>
2022-03-17 19:31:41 -0300

    adding plot dsig_dtheta

--- tests/core.py
+++ tests/core.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python3
+
+# import physical constants module
 import matplotlib.pyplot as plt
 import numpy as np
 from scipy.constants import epsilon_0, pi, e
@@ -38,6 +41,11 @@ def scattering_angle(b, r0, angle_unit='degrees'):
 
     return
 
+def scattering_differential(theta, r0):
+    dsig = np.pi*r0**2*np.cos(theta/2)
+    dsigdtheta = dsig/(4*np.sin(theta/2)**3)
+    return dsigdtheta
+
 # Calculations
 
 theta_in = np.linspace(0,180,100)[1:] # Scattering angle input.
@@ -46,24 +54,37 @@ b_out = impact_parameter(theta_in, r0) # Impact parameter calculated.
 b_in = np.linspace(0,30*r0,10000)[1:] # Impact parameter input.
 theta_out = scattering_angle(b_in, r0) # Scattering angle calculated.
 
+theta = np.linspace(-90, 90,100)[1:]
+dsig_dtheta = scattering_differential(theta, r0) #Differential scattering impact
+
 # Plots
 
 # b vs theta
 
 plt.figure(figsize=(8,6), facecolor='w')
 plt.plot(theta_in,b_out)
-plt.ylabel(r'$b$ [fm]',fontsize=14)
-plt.xlabel(r'$\theta$ [degrees]',fontsize=14)
-plt.title(r'Impact parameter as function of scattering angle',fontsize=16)
+plt.ylabel(r'b [$fm$]',fontsize=14)
+plt.xlabel(r'$\theta [graus]$',fontsize=14)
+plt.title(r'Distribuição de $b$ em função de $\theta$',fontsize=16)
 
-plt.savefig('plot_b_vs_theta.png', dpi=300, bbox_inches='tight')
+plt.savefig('grafico_b_vs_theta.png', dpi=300, bbox_inches='tight')
 
 # theta vs b
 
 plt.figure(figsize=(8,6), facecolor='w')
 plt.plot(b_in, theta_out)
-plt.ylabel(r'$\theta$ [degrees]',fontsize=14)
-plt.xlabel(r'$b$ [fm]',fontsize=14)
-plt.title(r'Scattering angle as function of impact parameter',fontsize=16)
+plt.ylabel(r'$\theta [graus]$',fontsize=14)
+plt.xlabel(r'b [$fm$]',fontsize=14)
+plt.title(r'Distribuição de $\theta$ em função de $b$',fontsize=16)
 
-plt.savefig('plot_theta_vs_b.png', dpi=300, bbox_inches='tight')
+plt.savefig('grafico_theta_vs_b.png', dpi=300, bbox_inches='tight')
+
+# dsig_dtheta vs theta
+
+plt.figure(figsize=(8,6), facecolor='w')
+plt.plot(dsig_dtheta,theta_in)
+plt.ylabel(r'$\theta [graus]$',fontsize=14)
+plt.xlabel(r'$d\sigma/d\theta$',fontsize=14)
+plt.title(r'Distribution of $\theta$ in function $d\sigma/d\theta$',fontsize=16)
+
+plt.savefig('grafico_theta_vs_dsgi_dtheta.png', dpi=300, bbox_inches='tight')
