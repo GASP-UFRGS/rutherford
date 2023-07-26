04507f4b923e2c9ec4868d7e4ebe98e68b86fbe1
kimcen <52336601+kimcen@users.noreply.github.com>
2023-07-25 22:31:42 -0300

    Added basic plotting functionality for Hoftstadter data
    Also made changes to the way the plot files are named

--- src/plot.py
+++ src/plot.py
@@ -1,6 +1,7 @@
 from card_reader import read_card, _raise_missing_card_error 
 import matplotlib.pyplot as plt
 import numpy as np
+import json
 
 def plot(output,card):
 
@@ -20,8 +21,16 @@ def plot(output,card):
 	
 
 	# Either cos, theta or omega. 
-	var = 'omega'
-	
+	var = 'theta'
+
+	# Set Hofstadter True or False
+	hof = True
+
+	# Read Hofstadter data 
+	with open("Hofstadter.json", "r") as json_file:
+	    real = json.load(json_file)
+
+
 	# b vs theta
 
 	plt.figure(figsize=(8,6), facecolor='w')
@@ -31,6 +40,7 @@ def plot(output,card):
 	plt.title('Impact parameter as function of the scattering angle',fontsize=16)
 
 	plt.savefig('plot_b_vs_theta.png', dpi=300, bbox_inches='tight')
+
 
 	# theta vs b
 
@@ -42,31 +52,50 @@ def plot(output,card):
 
 	plt.savefig('plot_theta_vs_b.png', dpi=300, bbox_inches='tight')
 
+
 	# differential cross section vs dtheta/dcos(theta)/domega
 
+	pltName='plot_dsig_'
 	plt.figure(figsize=(8,6), facecolor='w')
 	plt.plot(theta_in, dsig_dtheta, label='Rutherford')
 	if mott == "true":
 		plt.plot(theta_in, dsig_dtheta_Mott, label='Mott')
 	plt.yscale("log")
-	plt.legend()
 
-	# Change labels to cos 
+	# Change labels
 	if var == 'cos':
+		pltName += 'dcostheta_vs_costheta'
 		plt.xlabel(r'$cos(\theta)$'.format(unit=angUnit),fontsize=14)
 		plt.ylabel(r'$d\sigma/dcos(\theta)$',fontsize=14)
 		plt.title(r'Distribution of $d\sigma/dcos(\theta)$ as function of the scattering angle',fontsize=16)
-		plt.savefig('plot_dsig_dcostheta_vs_costheta.png', dpi=300, bbox_inches='tight')
+
 	elif var == 'theta': 
+		pltName += 'dtheta_vs_theta'
 		plt.xlabel(r'$\theta$ [{unit}]'.format(unit=angUnit),fontsize=14)
 		plt.ylabel(r'$d\sigma/d\theta$',fontsize=14)
 		plt.title(r'Distribution of $d\sigma/d\theta$ as function of the scattering angle',fontsize=16)
-		plt.savefig('plot_dsig_dtheta_vs_theta.png', dpi=300, bbox_inches='tight')
+		#Use hoftstadter data
+		if hof:
+			pltName += '_hoftstadter'
+			# Extract the x and y values from the JSON data
+			hofAngles = [float(entry["x"][0]["value"]) for entry in real["values"]]
+			dsigdOmega_300 = [float(entry["y"][0]["value"]) for entry in real["values"]]
+			dsigdOmega_400 = [float(entry["y"][1]["value"]) for entry in real["values"]]
+			dsigdOmega_500 = [float(entry["y"][2]["value"]) if entry["y"][2]["value"] != "-" else None for entry in real["values"]]
+			dsigdOmega_550 = [float(entry["y"][3]["value"]) for entry in real["values"]]
+
+			plt.plot(hofAngles, dsigdOmega_300, label='Hofstadter 300 MeV')
+			plt.plot(hofAngles, dsigdOmega_400, label='Hofstadter 400 MeV')
+			plt.plot(hofAngles, dsigdOmega_500, label='Hofstadter 500 MeV')
+			plt.plot(hofAngles, dsigdOmega_550, label='Hofstadter 550 MeV')
+		
 	elif var == 'omega':
+		pltName += 'domega_vs_theta'
 		plt.xlabel(r'$\theta$ [{unit}]'.format(unit=angUnit),fontsize=14)
 		plt.ylabel(r'$d\sigma/d\Omega$',fontsize=14)
 		plt.title(r'Distribution of $d\sigma/d\Omega$ as function of the scattering angle',fontsize=16)
-		plt.savefig('plot_dsig_domega_vs_theta.png', dpi=300, bbox_inches='tight')
+	plt.legend()
+	plt.savefig(pltName+',png', dpi=300, bbox_inches='tight')
 
 
 if __name__ == '__main__':
