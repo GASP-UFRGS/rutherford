548c549b1284c0704a0f5d398995d496d1fa3c4c
kimcen <kimenghusen@gmail.com>
2023-07-26 02:28:39 -0300

    Added code to normalize data to lowest angle of Rutherford scattering

--- src/plot.py
+++ src/plot.py
@@ -66,9 +66,23 @@ def plot(output,card_name):
 		plt.plot(theta_in, dsig_dtheta_Mott, label='Mott')
 	plt.yscale("log")
 
-	# Change labels
+	# Hoftstadter data: Extract the x and y values from the JSON data
+	hofAngles = [float(entry["x"][0]["value"]) for entry in real["values"]]
+
+
+	# Find closest theta to 30 degrees to normalize data 
+	minAngleDif = 180
+	for i in range(len(theta_in)):
+		angleDif = abs(theta_in[i] - 30.0)
+		if angleDif < minAngleDif:
+			minAngleDif = angleDif
+			minIndex = i
+
+
+	# Change axis labels
 	if var == 'cos':
 		pltName += 'dcostheta_vs_costheta'
+		hofAngles = np.cos(np.radians(hofAngles))
 		plt.xlabel(r'$cos(\theta)$'.format(unit=angUnit),fontsize=14)
 		plt.ylabel(r'$d\sigma/dcos(\theta)$',fontsize=14)
 		plt.title(r'Distribution of $d\sigma/dcos(\theta)$ as function of the scattering angle',fontsize=16)
@@ -85,37 +99,45 @@ def plot(output,card_name):
 		plt.ylabel(r'$d\sigma/d\Omega$',fontsize=14)
 		plt.title(r'Distribution of $d\sigma/d\Omega$ as function of the scattering angle',fontsize=16)
 
-		"""
-		Hoftstadter data
-		Data from 500MeV has a datapoint with zero uncertainty, so it is ignored
-		"""
-		# Extract the x and y values from the JSON data
-		hofAngles = [float(entry["x"][0]["value"]) for entry in real["values"]]
-		# Get energy value for labels
-		col_names = [E["value"] for E in real["qualifiers"]['E']]
 
-		if hof300 == 'true':
-			hof = True
-			dsigdOmega_300 = [float(entry["y"][0]["value"]) for entry in real["values"]]
-			plt.errorbar(hofAngles, dsigdOmega_300, yerr=[float(entry["y"][0]["errors"][0]["symerror"]) for entry in real["values"]], capsize = 3, ls='none', label="Hoftstadter "+col_names[0]) 
+	# Get energy value for labels
+	col_names = [E["value"] for E in real["qualifiers"]['E']]
+
+	if hof300 == 'true':
+		hof = True
+		dsigdOmega_300 = [float(entry["y"][0]["value"]) for entry in real["values"]]
+
+		normalizationFactor = dsig_dtheta[minIndex]/dsigdOmega_300[0]
+		dsigdOmega_300 = [point*normalizationFactor for point in dsigdOmega_300]
+
+		plt.errorbar(hofAngles, dsigdOmega_300, yerr=[float(entry["y"][0]["errors"][0]["symerror"]) for entry in real["values"]], capsize = 3, ls='none', label="Hoftstadter "+col_names[0]) 
 			
-		if hof400 == 'true':
-			hof = True
-			dsigdOmega_400 = [float(entry["y"][1]["value"]) for entry in real["values"]]
-			plt.errorbar(hofAngles, dsigdOmega_400, yerr=[float(entry["y"][1]["errors"][0]["symerror"]) for entry in real["values"]],  capsize = 3, ls='none', label="Hoftstadter "+col_names[1])
+	if hof400 == 'true':
+		hof = True
+		dsigdOmega_400 = [float(entry["y"][1]["value"]) for entry in real["values"]]
 
-		if hof500 == 'true':
-			hof = True
-			dsigdOmega_500 = [float(entry["y"][2]["value"]) for entry in real["values"] if entry["y"][2]["value"] != "-"]
-			plt.errorbar(hofAngles[1:], dsigdOmega_500, yerr=[float(entry["y"][2]["errors"][0]["symerror"]) for entry in real["values"] if entry["y"][2]["errors"][0]["symerror"] != 0], capsize = 3, ls='none', label="Hoftstadter "+col_names[2])
+		normalizationFactor = dsig_dtheta[minIndex]/dsigdOmega_400[0]
+		dsigdOmega_400 = [point*normalizationFactor for point in dsigdOmega_400]
 
-		if hof550 == 'true':
-			hof = True
-			dsigdOmega_550 = [float(entry["y"][3]["value"]) for entry in real["values"]]
-			plt.errorbar(hofAngles, dsigdOmega_550, yerr=[float(entry["y"][3]["errors"][0]["symerror"]) for entry in real["values"]],  capsize = 3, ls='none', label="Hoftstadter "+col_names[3])
+		plt.errorbar(hofAngles, dsigdOmega_400, yerr=[float(entry["y"][1]["errors"][0]["symerror"]) for entry in real["values"]],  capsize = 3, ls='none', label="Hoftstadter "+col_names[1])
+
+	# Data from 500MeV has a datapoint with zero uncertainty, so it is ignored
+	if hof500 == 'true':
+		hof = True
+		dsigdOmega_500 = [float(entry["y"][2]["value"]) for entry in real["values"] if entry["y"][2]["value"] != "-"]
+		plt.errorbar(hofAngles[1:], dsigdOmega_500, yerr=[float(entry["y"][2]["errors"][0]["symerror"]) for entry in real["values"] if entry["y"][2]["errors"][0]["symerror"] != 0], capsize = 3, ls='none', label="Hoftstadter "+col_names[2])
+
+	if hof550 == 'true':
+		hof = True
+		dsigdOmega_550 = [float(entry["y"][3]["value"]) for entry in real["values"]]
+
+		normalizationFactor = dsig_dtheta[minIndex]/dsigdOmega_550[0]
+		dsigdOmega_550 = [point*normalizationFactor for point in dsigdOmega_550]
+
+		plt.errorbar(hofAngles, dsigdOmega_550, yerr=[float(entry["y"][3]["errors"][0]["symerror"]) for entry in real["values"]],  capsize = 3, ls='none', label="Hoftstadter "+col_names[3])
 		
-		if hof == True:
-			pltName += '_hoftstadter'
+	if hof == True:
+		pltName += '_hoftstadter'
 
 	plt.legend()
 	plt.savefig(pltName, dpi=300, bbox_inches='tight')
