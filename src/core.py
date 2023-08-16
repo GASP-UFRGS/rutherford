ef99cc644c8b39f6ce375245da1a4f6b881bc3f5
kimcen <52336601+kimcen@users.noreply.github.com>
2023-08-16 11:14:42 -0300

    Added target recoil calculations and experimental data comparisons (#37)
    * Removed Hoftstadter data from 500 MeV
    
    * Separated data normalization logic into function
    
    * Added experimental data from 1953 Hoftstadter paper
    
    * Electron scattering logic
    
    * Added target recoil calculations
    
    * Added options for what to calculate on input card (Impact Parameter and/or Differential Cross Section)
    
    * Added tests to card_reader to check if inputs are of valid type
    
    * Changed output file writing and reading logic
    
    Updated and dorted requirement.txts to alphabetical order
    
    * Final fixes
    
    * Added Geiger and Marsden data

--- src/core.py
+++ src/core.py
@@ -2,13 +2,12 @@ import numpy as np
 import sys
 import periodictable
 from card_reader import read_card, _raise_missing_card_error 
-from scipy.constants import epsilon_0, pi, e, c
+from scipy.constants import epsilon_0, pi, e, c, alpha, hbar, electron_mass
 
 try:
     card_name = sys.argv[1]
 except IndexError:
     _raise_missing_card_error()
-
 
 # Constants
 
@@ -18,25 +17,30 @@ zTarget = parameters.get('zTarget')
 zProj = parameters.get('zProj') 
 angUnit = parameters.get('angUnit') 
 angStart = parameters.get('angStart') 
-angEnd = parameters.get('angEnd') 
+angEnd = parameters.get('angEnd')
 mott = parameters.get('mott') 
-recoil = parameters.get('recoil') 
-massTarget = parameters.get('massTarget') 
+recoil = parameters.get('recoil')
+impactParameter = parameters.get('impactParameter') 
+cross_section_variable = parameters.get('difCrossSec')
 
 # Outputs Nuclear mass in atomic mass units (u).
 element = periodictable.elements[zTarget]
 massTarget = element.mass
-
 massTarget = massTarget*1.6605402E-27 # Converts mass to kg
+
+
+# If projectile is electron
+if zProj != 0:
+    massProj = periodictable.elements[zProj].mass * 1.6605402E-27
+else:
+    zProj = 1
+    massProj = electron_mass
+
 
 kinEn = kinEn*e # Converts energy of incoming particles to Joules.
 kconst = 1/(4*pi*epsilon_0)
 fm = 1e15 # conversion factor to femtometer.
 D = (kconst*zProj*zTarget*e**2/kinEn) * fm # Minimum distance between incident particles and target in fm.
-
-
-# Either cos, theta or omega. 
-cross_section_variable = parameters.get('cross_section_variable')
 
 
 # Functions
@@ -57,7 +61,7 @@ def scattering_angle(bparam, D):
 
 
 
-def scattering_differential_Ruth(theta, D):
+def scattering_differential_Ruth(theta, D, cross_section_variable):
     """
     Returns differential scattering impact when given the scattering angle.
     """
@@ -67,60 +71,78 @@ def scattering_differential_Ruth(theta, D):
     if cross_section_variable == 'theta': 
         difCrossSec_Ruth = (D**2*pi*np.cos(theta/2)/(4*np.sin(theta/2)**3))
     
-    if cross_section_variable == 'omega':        
-        difCrossSec_Ruth = D**2/(16*np.sin(theta/2)**4)                                  
+    if cross_section_variable == 'omega':  
+        difCrossSec_Ruth = D**2/(16*np.sin(theta/2)**4)     
 
     return difCrossSec_Ruth
 
 
 
-def scattering_differential_Mott(theta, difCrossSec_Ruth, D):
+def scattering_differential_Mott(theta, difCrossSec_Ruth, kinEn, massTarget):
     """
     Returns differential scattering impact when given the scattering angle.
     """
-    if cross_section_variable == 'cos':
-        difCrossSec_Mott = difCrossSec_Ruth * ((1+np.cos(theta))/(2*(1+(((1-np.cos(theta))*kinEn)/(massTarget*c**2)))))
-
-    if cross_section_variable == 'theta':
-        difCrossSec_Mott = difCrossSec_Ruth * np.cos(theta/2)**2
-
-    if cross_section_variable == 'omega':
-        difCrossSec_Mott = difCrossSec_Ruth * np.cos(theta/2)**2   
+    difCrossSec_Mott = difCrossSec_Ruth * np.cos(theta/2)**2 
 
     return difCrossSec_Mott 
+
+def scattering_differential_Recoil(theta, difCrossSec_Mott, kinEn, massTarget):
+    """
+    Returns differential scattering impact when given the scattering angle.
+    """
+    difCrossSec_Recoil = difCrossSec_Mott * (1/(1+(((1-np.cos(theta))*kinEn)/(massTarget*c**2))))
+
+    return difCrossSec_Recoil
 
 
 # Calculations
 
 theta_in = np.linspace(angStart,angEnd,1000)[1:] # Scattering angle input.
 
-# Convert theta to radians if necessary
+# Converts theta to radians if necessary
 if angUnit == 'degrees':
     theta_in = np.radians(theta_in)
 
-b_out = impact_parameter(theta_in, D) # Impact parameter calculated.
-difCrossSec_Ruth = scattering_differential_Ruth(theta_in, D) #Differential scattering cross section.
+# Calculates Impact parameter
+if impactParameter == 'true':
+    b_out = impact_parameter(theta_in, D) 
 
+# Calculates diferential coss section
+if cross_section_variable in ['cos', 'theta', 'omega']:
 
-if mott == 'true':
-    difCrossSec_Mott = scattering_differential_Mott(theta_in, difCrossSec_Ruth, D) # Mott correction cross section.
+    difCrossSec_Ruth = scattering_differential_Ruth(theta_in, D, cross_section_variable) #Differential scattering cross section.
 
+    if mott == 'true':
+        difCrossSec_Mott = scattering_differential_Mott(theta_in, difCrossSec_Ruth, kinEn, massTarget) # Mott correction cross section.
 
-if cross_section_variable == 'cos':
-    theta_in = np.cos(theta_in)
-else:
-    theta_in = np.degrees(theta_in)
+    if recoil == "true":   
+         difCrossSec_Recoil = scattering_differential_Recoil(theta_in, difCrossSec_Mott, kinEn, massTarget) # Target recoil correction cross section.
 
 
 # Write to file
 
 file_path = "output.dat"
-data = np.column_stack((theta_in, b_out, difCrossSec_Ruth))
 
-if mott == "true":
-    data = np.column_stack((data, difCrossSec_Mott))
+header = 'theta'
+data = np.degrees(theta_in)
 
-np.savetxt(file_path, data, delimiter=",")
+if impactParameter == 'true':
+    header += ',b_out'
+    data = np.column_stack((data, b_out))
+
+if cross_section_variable in ['cos', 'theta', 'omega']:
+    header += ',difCrossSec_Ruth'
+    data = np.column_stack((data, difCrossSec_Ruth))
+
+    if mott == "true":
+        header += ',difCrossSec_Mott'
+        data = np.column_stack((data, difCrossSec_Mott))
+
+    if recoil == "true":
+        header += ',difCrossSec_Recoil'
+        data = np.column_stack((data, difCrossSec_Recoil))
+
+np.savetxt(file_path, data, delimiter=",", header = header, comments="")
 
 
 print("Data has been written to", file_path)
--- src/plot.py
+++ src/plot.py
@@ -1,89 +1,124 @@
 from card_reader import read_card, _raise_missing_card_error 
 import matplotlib.pyplot as plt
+import pandas as pd
 import numpy as np
 import json
 import sys
 
-def plot(output,card_name):
+def normalize_data(dataX, dataY, dataYerr, simulationX, simulationY, cross_section_variable):
+	# Normalizes data according to the first 
 
-	# Read file
-	data = np.loadtxt(output, delimiter=",")
+	minAngleDif = 180 # Arbitrary high initial value
+
+	if cross_section_variable == 'cos':
+		dataX = np.cos(np.radians(dataX))
+
+	# Search for simulation point with X closest to smallest value of dataX
+	angle_to_find = dataX[0]
+	for i in range(len(simulationX)):
+		angleDif = abs(simulationX[i] - angle_to_find )
+		if angleDif < minAngleDif:
+			minAngleDif = angleDif
+			minIndex = i
+ 
+	closestAngle = simulationY[minIndex]
+
+	normalizationFactor = closestAngle/dataY[0]
+	dataY = [point*normalizationFactor for point in dataY]
+
+	if dataYerr != None:
+		dataYerr = [entry * normalizationFactor for entry in dataYerr]
+
+		return dataX, dataY, dataYerr
+
+	return dataX, dataY
+
+
+def plot(output,card_name):
 
 	#Read parameter input card 
 	parameters = read_card(card_name)
 	kinEn = parameters.get('kinEn')
 	angUnit = parameters.get('angUnit')
 	mott = parameters.get('mott')
+	recoil = parameters.get('recoil')
+	impactParameter = parameters.get('impactParameter') 
+	cross_section_variable = parameters.get('difCrossSec')
+	hof25 = parameters.get('hoftstadter25') 
+	hof125 = parameters.get('hoftstadter125') 
 	hof300 = parameters.get('hoftstadter300')
 	hof400 = parameters.get('hoftstadter400')
-	hof500 = parameters.get('hoftstadter500')
 	hof550 = parameters.get('hoftstadter550')
-	hof = False
+	geiger = parameters.get('GeigerMarsden')
+	hof = any([hof25, hof125, hof300, hof400, hof550])
 	
-	theta_in = data[:, 0]  
-	b_out = data[:, 1]
-	dsig_dtheta = data[:, 2]
-	if mott == "true":
-		dsig_dtheta_Mott = data[:, 3]
+	# Read file
+	data = pd.read_csv(output)
+
+	# Save each column of output in a dictionary with the header as keys
+	column_lists = {}
+	for column in data.columns:
+	    column_lists[column] = data[column].tolist()
+
+	# Place values on variables
+	theta_in = column_lists['theta']
+
+	if cross_section_variable in ['cos', 'theta', 'omega']:
+		difCrossSec_Ruth = column_lists['difCrossSec_Ruth']
+		if mott == "true":
+			difCrossSec_Mott = column_lists['difCrossSec_Mott']
+		if recoil == "true":
+			difCrossSec_Recoil = column_lists['difCrossSec_Recoil']
+
+	if impactParameter == 'true':
+		b_out = column_lists['b_out']
+
+		# b vs theta
+	
+		plt.figure(figsize=(8,6), facecolor='w')
+		plt.plot(theta_in, b_out)
+		plt.ylabel(r'$b$ [fm]',fontsize=14)
+		plt.xlabel(r'$\theta$ [{unit}]'.format(unit=angUnit),fontsize=14)
+		plt.title('Impact parameter as function of the scattering angle',fontsize=16)
+		pltName = 'plot_b_vs_theta.png'
+		plt.savefig(pltName, dpi=300, bbox_inches='tight')
+		print(f'Created {pltName}')
 	
 
-	# Either cos, theta or omega. 
-	cross_section_variable = parameters.get('cross_section_variable')
+		# theta vs b
 
+		plt.figure(figsize=(8,6), facecolor='w')
+		plt.plot(b_out, theta_in)
+		plt.ylabel(r'$\theta$ [{unit}]'.format(unit=angUnit),fontsize=14)
+		plt.xlabel(r'$b$ [fm]',fontsize=14)
+		plt.title('Scattering angle as function of the impact parameter',fontsize=16)
+		pltName = 'plot_theta_vs_b.png'
+		plt.savefig(pltName, dpi=300, bbox_inches='tight')
+		print(f'Created {pltName}')
 
-	# Read Hofstadter data 
-	with open(sys.path[0] + '/../data/Hofstadter.json', 'r') as json_file:
-	    real = json.load(json_file)
-
-
-	# b vs theta
-	
-	plt.figure(figsize=(8,6), facecolor='w')
-	plt.plot(theta_in, b_out)
-	plt.ylabel(r'$b$ [fm]',fontsize=14)
-	plt.xlabel(r'$\theta$ [{unit}]'.format(unit=angUnit),fontsize=14)
-	plt.title('Impact parameter as function of the scattering angle',fontsize=16)
-
-	plt.savefig('plot_b_vs_theta.png', dpi=300, bbox_inches='tight')
-
-
-	# theta vs b
-
-	plt.figure(figsize=(8,6), facecolor='w')
-	plt.plot(b_out, theta_in)
-	plt.ylabel(r'$\theta$ [{unit}]'.format(unit=angUnit),fontsize=14)
-	plt.xlabel(r'$b$ [fm]',fontsize=14)
-	plt.title('Scattering angle as function of the impact parameter',fontsize=16)
-
-	plt.savefig('plot_theta_vs_b.png', dpi=300, bbox_inches='tight')
 
 
 	# differential cross section vs dtheta/dcos(theta)/domega
 
+	if cross_section_variable == 'cos':
+		theta_in = np.cos(np.radians(theta_in))
+
 	pltName='plot_dsig_'
 	plt.figure(figsize=(8,6), facecolor='w')
-	plt.plot(theta_in, dsig_dtheta, label='Rutherford')
+	plt.plot(theta_in, difCrossSec_Ruth, label='Rutherford')
+
 	if mott == "true":
-		plt.plot(theta_in, dsig_dtheta_Mott, label='Mott')
+		plt.plot(theta_in, difCrossSec_Mott, label='Mott')
+
+	if recoil == "true":
+		plt.plot(theta_in, difCrossSec_Recoil, label='Target Recoil')
+
 	plt.yscale("log")
-
-	# Hoftstadter data: Extract the x and y values from the JSON data
-	hofAngles = [float(entry["x"][0]["value"]) for entry in real["values"]]
-
-
-	# Find closest theta to 30 degrees to normalize data 
-	minAngleDif = 180
-	for i in range(len(theta_in)):
-		angleDif = abs(theta_in[i] - 30.0)
-		if angleDif < minAngleDif:
-			minAngleDif = angleDif
-			minIndex = i
 
 
 	# Change axis labels
 	if cross_section_variable == 'cos':
 		pltName += 'dcostheta_vs_costheta'
-		hofAngles = np.cos(np.radians(hofAngles))
 		plt.xlabel(r'$cos(\theta)$'.format(unit=angUnit),fontsize=14)
 		plt.ylabel(r'$d\sigma/dcos(\theta)$',fontsize=14)
 		plt.title(r'Distribution of $d\sigma/dcos(\theta)$ as function of the scattering angle',fontsize=16)
@@ -101,55 +136,119 @@ def plot(output,card_name):
 		plt.title(r'Distribution of $d\sigma/d\Omega$ as function of the scattering angle',fontsize=16)
 
 
+	# Read Hofstadter data 
+	if any([hof300, hof400, hof550]):
+		json_file = open(sys.path[0] + '/../data/Hofstadter.json', 'r')
+		real = json.load(json_file)
+
+	# Extract the x values from the JSON data
+	hofAngles = [float(entry["x"][0]["value"]) for entry in real["values"]]
+
 	# Get energy value for labels
 	col_names = [E["value"] for E in real["qualifiers"]['E']]
 
+
+	# Plotting of Hoftstadter data
+	if hof25 == 'true':
+		if kinEn != 25e6:
+			print(f"Are you sure you want to plot data with 125 MeV? Chosen kinetic energy {int(kinEn*1e-6)}MeV is not 25 MeV!")
+
+		hof_difCrossSec_25 = []
+		hofAngles25 = []
+		with open(sys.path[0] + '/../data/hoftstadter25.csv', 'r') as hof25:
+			for line in hof25:
+				angle, value = line.strip().split(';')
+				hofAngles25.append(float(angle))
+				hof_difCrossSec_25.append(float(value))
+
+		# Normalizing data 
+		hofAngles25, hof_difCrossSec_25 = normalize_data(hofAngles25, hof_difCrossSec_25, None, theta_in, difCrossSec_Ruth, cross_section_variable)
+
+		plt.scatter(hofAngles25, hof_difCrossSec_25, label="Hoftstadter 25 Mev") 
+
+	if hof125 == 'true':
+		if kinEn != 125e6:
+			print(f"Are you sure you want to plot data with 125 MeV? Chosen kinetic energy {int(kinEn*1e-6)}MeV is not 125 MeV!")
+		
+		hof_difCrossSec_125 = []
+		hofAngles125 = []
+		with open(sys.path[0] + '/../data/hoftstadter125.csv', 'r') as hof125:
+			for line in hof125:
+				angle, value = line.strip().split(';')
+				hofAngles125.append(float(angle))
+				hof_difCrossSec_125.append(float(value))
+
+		# Normalizing data 
+		hofAngles125, hof_difCrossSec_125 = normalize_data(hofAngles125, hof_difCrossSec_125, None, theta_in, difCrossSec_Ruth, cross_section_variable)
+
+		plt.scatter(hofAngles125, hof_difCrossSec_125, marker='s', color='black',  label="Hoftstadter 125 Mev") 
+
+
 	if hof300 == 'true':
-		if kinEn != 3e6:
-			print('Are you sure you want to plot data with 300MeV? Chosen kinetic energy (kinEn) is not 300Mev!')
-		hof = True
-		dsigdOmega_300 = [float(entry["y"][0]["value"]) for entry in real["values"]]
+		if kinEn != 300e6:
+			print(f"Are you sure you want to plot data with 300 MeV? Chosen kinetic energy {int(kinEn*1e-6)}MeV is not 300 MeV!")
 
-		normalizationFactor = dsig_dtheta[minIndex]/dsigdOmega_300[0]
-		dsigdOmega_300 = [point*normalizationFactor for point in dsigdOmega_300]
+		hof_difCrossSec_300 = [float(entry["y"][0]["value"]) for entry in real["values"]]
+		yerr = [float(entry["y"][2]["errors"][0]["symerror"]) for entry in real["values"]]
 
-		plt.errorbar(hofAngles, dsigdOmega_300, yerr=[float(entry["y"][0]["errors"][0]["symerror"]) for entry in real["values"]], capsize = 3, ls='none', label="Hoftstadter "+col_names[0]) 
-			
+		# Normalizing data 
+		hofAngles, hof_difCrossSec_300, yerr = normalize_data(hofAngles, hof_difCrossSec_300, yerr, theta_in, difCrossSec_Ruth, cross_section_variable)
+
+		plt.errorbar(hofAngles, hof_difCrossSec_300, yerr, capsize = 3, ls='none', label="Hoftstadter "+col_names[0]) 
+		
+
 	if hof400 == 'true':
-		if kinEn != 4e6:
-			print('Are you sure you want to plot data with 400MeV? Chosen kinetic energy (kinEn) is not 400Mev!')
-		hof = True
-		dsigdOmega_400 = [float(entry["y"][1]["value"]) for entry in real["values"]]
+		if kinEn != 400e6:
+			print(f"Are you sure you want to plot data with 400 MeV? Chosen kinetic energy {int(kinEn*1e-6)}MeV is not 400 MeV!")
 
-		normalizationFactor = dsig_dtheta[minIndex]/dsigdOmega_400[0]
-		dsigdOmega_400 = [point*normalizationFactor for point in dsigdOmega_400]
+		hof_difCrossSec_400 = [float(entry["y"][1]["value"]) for entry in real["values"]]
+		yerr = [float(entry["y"][2]["errors"][0]["symerror"]) for entry in real["values"]]
 
-		plt.errorbar(hofAngles, dsigdOmega_400, yerr=[float(entry["y"][1]["errors"][0]["symerror"]) for entry in real["values"]],  capsize = 3, ls='none', label="Hoftstadter "+col_names[1])
+		# Normalizing data 
+		hofAngles, hof_difCrossSec_400, yerr = normalize_data(hofAngles, hof_difCrossSec_400, yerr, theta_in, difCrossSec_Ruth, cross_section_variable)
 
-	# Data from 500MeV has a datapoint with zero uncertainty, so it is ignored
-	if hof500 == 'true':
-		if kinEn != 5e6:
-			print('Are you sure you want to plot data with 500MeV? Chosen kinetic energy (kinEn) is not 500Mev!')
-		hof = True
-		dsigdOmega_500 = [float(entry["y"][2]["value"]) for entry in real["values"] if entry["y"][2]["value"] != "-"]
-		plt.errorbar(hofAngles[1:], dsigdOmega_500, yerr=[float(entry["y"][2]["errors"][0]["symerror"]) for entry in real["values"] if entry["y"][2]["errors"][0]["symerror"] != 0], capsize = 3, ls='none', label="Hoftstadter "+col_names[2])
+		plt.errorbar(hofAngles, hof_difCrossSec_400, yerr, capsize = 3, ls='none', label="Hoftstadter "+col_names[1])
+	
 
 	if hof550 == 'true':
-		if kinEn != 5.5e6:
-			print('Are you sure you want to plot data with 550MeV? Chosen kinetic energy (kinEn) is not 550Mev!')
-		hof = True
-		dsigdOmega_550 = [float(entry["y"][3]["value"]) for entry in real["values"]]
+		if kinEn != 550e6:
+			print(f"Are you sure you want to plot data with 550 MeV? Chosen kinetic energy {int(kinEn*1e-6)}MeV is not 550 MeV!")
 
-		normalizationFactor = dsig_dtheta[minIndex]/dsigdOmega_550[0]
-		dsigdOmega_550 = [point*normalizationFactor for point in dsigdOmega_550]
+		hof_difCrossSec_550 = [float(entry["y"][3]["value"]) for entry in real["values"]]
+		yerr = [float(entry["y"][3]["errors"][0]["symerror"]) for entry in real["values"]]
 
-		plt.errorbar(hofAngles, dsigdOmega_550, yerr=[float(entry["y"][3]["errors"][0]["symerror"]) for entry in real["values"]],  capsize = 3, ls='none', label="Hoftstadter "+col_names[3])
-		
-	if hof == True:
+		# Normalizing data
+		hofAngles, hof_difCrossSec_550, yerr = normalize_data(hofAngles, hof_difCrossSec_550, yerr, theta_in, difCrossSec_Ruth, cross_section_variable)
+
+		plt.errorbar(hofAngles, hof_difCrossSec_550, yerr, capsize = 3, ls='none', label="Hoftstadter "+col_names[3])
+
+
+	if any([hof25, hof125, hof300, hof400, hof550]):
 		pltName += '_hoftstadter'
 
+
+	if geiger == 'true':
+
+		geiger_difCrossSec_125 = []
+		geigerAngles125 = []
+		with open(sys.path[0] + '/../data/GeigerMarsden.csv', 'r') as hof125:
+			for line in hof125:
+				angle, value = line.strip().split(';')
+				geigerAngles125.append(float(angle))
+				geiger_difCrossSec_125.append(float(value))
+
+		# Normalizing data 
+		geigerAngles125, geiger_difCrossSec_125 = normalize_data(geigerAngles125, geiger_difCrossSec_125, None, theta_in, difCrossSec_Ruth, cross_section_variable)
+
+		plt.scatter(geigerAngles125, geiger_difCrossSec_125, color='black',  label="GeigerMarsden") 
+
+		pltName += '_GeigerMarsden'
+
+
+	pltName += f'_{int(kinEn*1e-6)}MeV'
 	plt.legend()
 	plt.savefig(pltName, dpi=300, bbox_inches='tight')
+	print(f'Created {pltName}.png')
 
 
 if __name__ == '__main__':
